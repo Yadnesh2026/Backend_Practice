48147ef41272f7cc594cad459ec517183c08c058
Vedant <vvidulkar@gmail.com>
2026-05-07 22:16:42 +0530

    register

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "The user is already Registered" });
+  }
+
+  const newUser = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+res.status(201).json({
+    id: newUser._id,
+    name: newUser.name,
+    email: newUser.email
+});
+};
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const Todo = require("../models/Todo");
 const asyncHandler = require("../utils/asyncHandler")
 
