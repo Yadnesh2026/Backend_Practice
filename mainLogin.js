a93c16a691a3c07c3e74bd4eae595f235ee1be57
Vedant <vvidulkar@gmail.com>
2026-05-11 00:22:48 +0530

    login successs

--- mainLogin.js
+++ mainLogin.js
@@ -1,15 +1,34 @@
-const mainLogin = document.getElementById("mainLogin")
-const email = document.getElementById("email")
+const mainLogin = document.getElementById("mainLogin");
+const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+mainLogin.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-mainLogin.addEventListener("submit",async (e)=>{
-    e.preventDefault();
+  const emailValue = email.value;
+  const passwordValue = password.value;
 
-    const emailValue = email.value;
-    const passwordValue = password.value;
+  console.log(emailValue, passwordValue);
 
-    console.log(emailValue,passwordValue)
+  const dataFetech = await fetch("http://localhost:7111/api/login", {
+    method: "POST",
 
-    const dataFetech = await fetch("http://localhost:7111/api/login")
-})
+    body: JSON.stringify({
+      email: emailValue,
+      password: passwordValue,
+    }),
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await dataFetech.json();
+  console.log(data);
+
+  if(dataFetech.ok){
+    console.log("User is Registred")
+  }else{
+    console.log(data.message)
+  }
+});
