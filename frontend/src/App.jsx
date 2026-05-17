0349ba8ef4ce13899a7d4213c90b2021ad452075
Vedant <vvidulkar@gmail.com>
2026-05-18 01:07:23 +0530

    add api auth example

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,32 @@
 import { use, useState } from "react";
 
-
 //Input Vaue Questions
 function App() {
-  const [input, setInput] = useState("");
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] =useState("")
 
-  return (
-    <>
-      <h1>{name}</h1>
-      <input
-        placeholder="Enter the name"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        type="text"
-      />
+  const handleSubmit =(e)=>{
+    e.preventDefault()
 
-      <button onClick={() => setName(input)}>Submit</button>
-    </>
-  );
+    console.log(email)
+    console.log(password)
+
+
+  }
+
+  return <>
+  <form onChange={handleSubmit}>
+
+    <input placeholder="Enter Email" onChange={(e)=> setEmail(e.target.value)} value={email}/>
+    <input placeholder="Enter Password" onChange={(e)=> setPassword(e.target.value) value={password}}/>
+
+    <button type="submit">Submit</button>
+
+  </form>
+
+
+  </>;
 }
 
 export default App;
-
-
-
-
